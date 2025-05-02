645c4cbb2fa4c801eb0749a8714838f0f89caf4e
Macielsbdev <macielbezerra2018@gmail.com>
2025-05-02 17:06:43 -0300

    projeto crm

---
+++ index.js
@@ -0,0 +1,21 @@
+function mostrarOverlay() {
+    let overlayElement = document.querySelector("#overlay");
+    let gavetaElement = document.querySelector("#gaveta");
+    if (overlayElement.classList.contains("invisible")) {
+        overlayElement.classList.remove("invisible", "opacity-0");
+        gavetaElement.classList.remove("-right-ful");
+        gavetaElement.classList.add("right-0");
+
+    } else {
+        overlayElement.classList.add("invisible", "opacity-0");
+        gavetaElement.classList.add("-right-full");
+        gavetaElement.classList.remove("right-0");
+
+    }
+
+
+
+
+}
+
+    
