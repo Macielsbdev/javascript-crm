a27c01c50464dbfadb0c2a500ede0fa1ceb8ec5f
Macielsbdev <macielbezerra2018@gmail.com>
2025-05-05 17:07:20 -0300

    fiz atividades crm

---
+++ clientes.js
@@ -0,0 +1,27 @@
+let clientes = [
+    {
+        nome: "Conrado",
+        email: "conrado@gmail.com",
+        telefone: "(85) 00000 0000",
+        dta: "05/05/2025"
+    },
+    {
+        nome: "Emanuel",
+        email: "amanuel@gmail.com",
+        telefone: "(85) 00000 0000",
+        dta: "05/05/2025"
+    },
+    {
+        nome: "Ana",
+        email: "ana@gmail.com",
+        telefone: "(85) 00000 0000",
+        dta: "05/05/2025"
+    }
+];
+
+function carregarClientes(ListaDeClientes){
+    let tbodyElement = document.querySelector("#tabela");
+    ListaDeClientes.map((cliente) => {
+
+    })
+}
