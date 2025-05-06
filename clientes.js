ecb3e5d2a930ce41e6fa0fc62c35021b36367991
Macielsbdev <macielbezerra2018@gmail.com>
2025-05-05 22:08:39 -0300

    fiz uma questão de tabela

--- clientes.js
+++ clientes.js
@@ -16,12 +16,31 @@ let clientes = [
         email: "ana@gmail.com",
         telefone: "(85) 00000 0000",
         dta: "05/05/2025"
+    },
+    {
+        nome: "Maciel",
+        email: "maciel@gmail.com",
+        telefone: "(85) 00000 0000",
+        dta: "05/05/2025"
     }
 ];
 
 function carregarClientes(ListaDeClientes){
     let tbodyElement = document.querySelector("#tabela");
     ListaDeClientes.map((cliente) => {
+        tbodyElement.innerHTML += `
+         <tr class="*:leading-[40px]">
+                    <td>${cliente.nome}</td>
+                    <td>${cliente.email}</td>
+                    <td>${cliente.telefone}</td>
+                    <td>${cliente.dta}</td>
 
+                    <td class="w-[100px] flex justify-center gap-4">
+                        <box-icon name="pencil"></box-icon>
+                        <box-icon name="trash"></box-icon>
+                    </td>
+                </tr>
+`
     })
 }
+carregarClientes(clientes);
