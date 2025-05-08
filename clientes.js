f3524b605e53ae5a7b45b05aa21da34b96f659f7
Macielsbdev <macielbezerra2018@gmail.com>
2025-05-08 16:36:38 -0300

    fiz atividade de requisição

--- clientes.js
+++ clientes.js
@@ -22,19 +22,19 @@
 
 // ];
 
-let clientes = sessionStorage.getItem("clientes") ? JSON.parse(sessionStorage.getItem("clientes")) : [];
+let clientes = [];
 
-function buscarClientes(){
+function buscarClientes() {
     fetch("http://localhost:3000/clientes")
-    .then((res) => res.json())
-    .then((lista) => {
-        clientes = lista;
-        carregarClientes(clientes);
+        .then((res) => res.json())
+        .then((lista) => {
+            clientes = lista;
+            carregarClientes(clientes);
 
-    })
+        })
 }
 
-buscarClientes();
+
 
 
 function carregarClientes(listaDeClientes) {
@@ -43,31 +43,57 @@ function carregarClientes(listaDeClientes) {
     listaDeClientes.map((cliente) => {
         tbodyElement.innerHTML += `
             <tr class="*:leading-[40px] text-center">
+                <td>${cliente.id}</td>
                 <td>${cliente.nome}</td>
                 <td>${cliente.email}</td>
                 <td>${cliente.telefone}</td>
                 <td>${cliente.data}</td>
                 <td class="w-[100px] flex justify-center gap-4">
                     <box-icon name="pencil"></box-icon>
-                    <box-icon name="trash"></box-icon>
+                    <box-icon name="trash" onclick="deletarCliente('${cliente.id}')"></box-icon>
                 </td>
             </tr>
         `;
     })
 }
 
-carregarClientes(clientes);
 
-function cadastrarCliente(form){
+function cadastrarCliente(form) {
     event.preventDefault();
 
     // vão pegar os valores dos inputs do formulario e transformar em um objeto
     let formData = new FormData(form);
     let cliente = Object.fromEntries(formData.entries());
-    
+
     // inserir o novo cliente no final do arrray clientes
-    clientes.push(cliente);
-    sessionStorage.setItem("clientes", JSON.stringify(clientes));
-    mostrarOverlay();
-    carregarClientes(clientes);
+
+    //inserir o novo cliente no final do array clientes
+    fetch("http://localhost:3000/clientes", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+
+        },
+        body:JSON.stringify(cliente)
+    })
+    .then(res => res.json())
+    .then(() => {
+        alert("Registro cadastrado com sucesso!")
+        mostrarOverlay();
+        carregarClientes(clientes);
+    })
 }
+
+function deletarCliente(id) {
+    fetch(`http://localhost:3000/clientes/${id}`, {
+        method: "DELETE"
+    })
+        .then(res => res.json())
+        .then(() => {
+            alert(`Item ${id} apagado!`);
+
+        })
+
+}
+
+buscarClientes();
